634fccfa8d14bf45409b9c66cbea3f76f76ddd02
Ayush Soni <61859852+ayush176@users.noreply.github.com>
2024-02-26 01:06:21 +0530

    Update Kruskal's Algorithm for MST.cpp
    added example of unionByRank and unionBySize

--- Kruskal's Algorithm for MST.cpp
+++ Kruskal's Algorithm for MST.cpp
@@ -68,3 +68,88 @@ int main() {
     }
     return 0;
 }
+
+
+////////////////GFG find min spanning tree////////////
+class DSU{
+    private:
+    vector<int> par,rank,size;
+    
+    public:
+    DSU(int n){
+        par.resize(n+1);
+        rank.resize(n+1,0);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;i++){
+            par[i]=i;
+        }
+    }
+    
+    int findPar(int node){
+        if(par[node]==node)return node;
+        return par[node] = findPar(par[node]);
+    }
+    
+    void unionByRank(int a, int b){
+        int up_a = findPar(a);
+        int up_b = findPar(b);
+        
+        if(up_a != up_b){
+            if(rank[up_a] < rank[up_b]){
+                par[up_a] = up_b;
+            } else if(rank[up_b] < rank[up_a]){
+                par[up_b] = up_a;
+            } else {
+                par[up_a] = up_b;
+                rank[up_b]++;
+            }
+        }
+    }
+    
+    void unionBySize(int a, int b){
+        int up_a = findPar(a);
+        int up_b = findPar(b);
+        
+        if(size[up_a] < size[up_b]){
+            par[up_a] = up_b;
+            size[up_b] += size[up_a];
+        } else {
+            par[up_b] = up_a;
+            size[up_a]+= size[up_b];
+        }
+    }
+    
+};
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        // code here
+        vector<pair<int,pair<int,int>>> edges;
+        for(int node = 0;node<V;node++){
+            for(auto it : adj[node]){
+                int u = it[0];
+                int wt = it[1];
+                edges.push_back({wt,{u,node}});
+            }
+        }
+        sort(edges.begin(),edges.end());
+        
+        int ans = 0;
+        DSU d(V);
+        
+        for(auto it: edges){
+            int a = it.second.first;
+            int b = it.second.second;
+            int wt = it.first;
+            
+            if(d.findPar(a) != d.findPar(b)){
+                ans += wt;
+                d.unionBySize(a,b);
+            }
+        }
+        return ans;
+    }
+};
